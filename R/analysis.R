6f1d65617a2531aa43ce745c3ae80881b6f6b55d
amalthomas111 <amalthomas111@gmail.com>
2022-06-16 15:15:39 -0700

    update vignette added analysis scripts

---
+++ R/analysis.R
@@ -0,0 +1,113 @@
+#' Function to create short name for species
+#'
+#' @param species_list a vector of species name
+#'
+#' @return short version of species name
+#' @export
+#'
+#' @examples
+#' species <- c("Homo_sapiens", "Mus_musculus")
+#' species_short <- sapply(species, getSpeciesShortName)
+#' species <- "Homo_sapiens"
+#' getSpeciesShortName(species)
+getSpeciesShortName <- function(species_list) {
+  shortname <- c()
+  for (sp in species_list) {
+    if (!grepl("_", sp)) {
+      cat("\nspecies:", sp, "\n")
+      stop("Invalid species full name: No '_' found!")
+    }
+    shortname <- c(shortname, paste0(tolower(substr(base::strsplit(sp, "_")[[1]][1],
+                                                    1, 1)),
+                                     base::strsplit(sp, "_")[[1]][2]))
+  }
+  return(shortname)
+}
+#' Function to return scientific name of species
+#'
+#' @param x species name
+#'
+#' @return species name in scientific format
+#' @export
+#'
+#' @examples
+#' species <- "Homo_sapiens"
+#' getScientificName(species)
+getScientificName <- function(x) {
+  if (!grepl("_", x)) {
+    cat("\nspecies:", x, "\n")
+    stop("Invalid species full name: No '_' found!")
+  }
+  parts <- base::strsplit(x, "_")[[1]]
+  return(paste0(substr(x, 1, 1), ".", parts[2]))
+}
+#' Calculate TPM normalization for a vector
+#'
+#' @param x a column of gene expression profiles
+#' @param gene_len gene lengths (kb)
+#'
+#' @return
+#' @export
+#'
+#' @examples
+countToTpm <- function(x, gene_len) {
+  rate <- log(x) - log(gene_len)
+  denom <- log(sum(exp(rate)))
+  exp(rate - denom + log(1e6))
+}
+#' TPM normalization for a gene expression matrix
+#'
+#' @param rawCounts Raw gene expression counts matrix
+#' @param gene_len Matching gene lengths (kb)
+#'
+#' @return TPM normalized matrix
+#' @export
+#'
+#' @examples
+normalizeTPM <- function(rawCounts, gene_len = NULL) {
+  common_genes <- intersect(rownames(rawCounts), names(gene_len))
+  rawCounts <- rawCounts[common_genes, , drop = F]
+  gene_len <- gene_len[common_genes]
+  glen <- gene_len[match(toupper(rownames(rawCounts)),
+                            toupper(names(gene_len)))]
+  if (nrow(rawCounts) == length(gene_len)) {
+    cat("\nTPM counts returned")
+    df <- as.data.frame(apply(rawCounts, 2, function(col) {
+       countToTpm(x = col, gene_len = glen)}))
+    df <- round(df, 4)
+  } else {
+    stop ("\tUnequal number of genes for TPM calculation")
+  }
+}
+
+#' Function to calculate mean expression values for each tissue/group
+#'
+#' @param counts gene expression matrix
+#' @param metadata data frame with species tissue key column
+#' @param ndecimal number of decimals to round
+#'
+#' @return
+#' @export
+#'
+#' @examples
+calcAvgCounts <- function(counts, metadata, ndecimal = 4) {
+  counts_avg <- list()
+  if ("key" %in% colnames(metadata)) {
+    for (species_organ in sort(unique(metadata$key))) {
+      nlibs <- length(colnames(counts)[grepl(species_organ,
+                                                  colnames(counts))])
+      if (nlibs > 1)
+        counts_avg[[species_organ]] <- round(rowMeans(counts[, colnames(counts)[
+          grepl(species_organ, colnames(counts))]],
+          dim = 1), ndecimal)
+      else if (nlibs == 1)
+        counts_avg[[species_organ]] <- round(counts[, colnames(counts)[grepl(
+          species_organ, colnames(counts))], drop = T], ndecimal)
+    }
+    avg_counts <- as.data.frame(do.call(cbind, counts_avg))
+    #row.names(avg_counts) <- row.names(counts)
+    return(avg_counts)
+  } else {
+    stop("'key' column not found in the metadata!Exiting!")
+  }
+}
